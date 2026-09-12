8535394cee9c21ad78c787363b0983648e0a811b
X-aism <X-aism@users.noreply.github.com>
2026-09-12 13:24:18 +0800

    添加 GitHub Pages 自动部署流水线

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  // 部署到 GitHub Pages 的子路径：https://x-aism.github.io/resume-generator/
+  base: '/resume-generator/',
 })
